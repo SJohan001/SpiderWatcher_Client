b1f40549ade06e7e63b33fb773c78c086f2c3d5a
NoSoyPablox <pablohernan298@gmail.com>
2024-06-12 12:51:36 -0600

    DTOS añadidos para crear y validar usuario

---
+++ SpiderWatcher_Client/DTOs/UserDTO/CreateUserDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace SpiderWatcher_Client.DTOs.UserDTO
+{
+    public class CreateUserDTO
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateOnly DateBirth { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
