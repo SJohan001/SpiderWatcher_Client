64cb657de0b4062534f8e2e22e92f558759f5a39
NoSoyPablox <pablohernan298@gmail.com>
2024-06-13 16:09:30 -0600

    edicion de program

--- SpiderWatcher_Client/Program.cs
+++ SpiderWatcher_Client/Program.cs
@@ -6,6 +6,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+var grpcBaseDeAddress = "https://puer.azurewebsites.net";
+var blobStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=spiderwatcherbs;AccountKey=fi8pH/Jt65gddZqKXB+SAXf0F2hGAtqksmRH7j6jBVqFhzqTKGu3tPT2TV0oD0edMo8HlbYqWSJ9+ASt/3odtQ==;EndpointSuffix=core.windows.net";
+var containerName = "imagecontent";
+
 var apiBaseDeAddress = "http://localhost:8088";
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseDeAddress) });
 
