1d17da1e86d911c57ac764e04f30875b579e7916
NoSoyPablox <pablohernan298@gmail.com>
2024-06-14 23:53:24 -0600

    Obtener video

---
+++ SpiderWatcher_Client/Conexion/AzureVideoService.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Storage.Blobs;
+
+namespace SpiderWatcher_Client.Conexion
+{
+    public class AzureVideoService
+    {
+        private readonly string _blobStorageConnectionString;
+        private readonly string _containerName;
+
+        public AzureVideoService(string blobStorageConnectionString, string containerName)
+        {
+            _blobStorageConnectionString = blobStorageConnectionString;
+            _containerName = containerName;
+        }
+
+        public async Task<byte[]> GetVideoAsync(string videoName)
+        {
+            var blobServiceClient = new BlobServiceClient(_blobStorageConnectionString);
+            var blobContainerClient = blobServiceClient.GetBlobContainerClient(_containerName);
+            var blobClient = blobContainerClient.GetBlobClient(videoName);
+
+            using (var memoryStream = new MemoryStream())
+            {
+                await blobClient.DownloadToAsync(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
--- SpiderWatcher_Client/Program.cs
+++ SpiderWatcher_Client/Program.cs
@@ -8,10 +8,13 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var grpcBaseDeAddress = "https://puer.azurewebsites.net";
+var grpcBaseDeAddress = "https://grcpsw.azurewebsites.net";
 var blobStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=spiderwatcherbs;AccountKey=fi8pH/Jt65gddZqKXB+SAXf0F2hGAtqksmRH7j6jBVqFhzqTKGu3tPT2TV0oD0edMo8HlbYqWSJ9+ASt/3odtQ==;EndpointSuffix=core.windows.net";
-var containerName = "imagecontent";
-builder.Services.AddScoped(sp => new AzureBlobService(blobStorageConnectionString, containerName));
+var imageContainerName = "imagecontent";
+var videoContainerName = "videocontent";
+builder.Services.AddScoped(sp => new AzureBlobService(blobStorageConnectionString, imageContainerName));
+
+builder.Services.AddScoped(sp => new AzureVideoService(blobStorageConnectionString, videoContainerName ));
 
 var apiBaseDeAddress = "http://localhost:8088";
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseDeAddress) });
